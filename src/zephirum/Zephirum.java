41ab83afb4443deccca007d19e16c7f1df82effc
Redmountain0 <65642577+Redmountain0@users.noreply.github.com>
2022-10-11 10:45:27 +0900

    added assets

--- src/zephirum/Zephirum.java
+++ src/zephirum/Zephirum.java
@@ -19,11 +19,11 @@ public class Zephirum extends Mod{
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                BaseDialog dialog = new BaseDialog("startDialog");
+                dialog.cont.add("SANS").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.cont.image(Core.atlas.find("zephirum-frog")).pad(20f).row();
+                dialog.cont.button("Okay", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
