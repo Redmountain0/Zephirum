5df6bfe8a7f87086fee2724da299a93615924f02
Redmountain0 <65642577+Redmountain0@users.noreply.github.com>
2022-10-11 17:21:42 +0900

    better spiron priority

--- src/zephirum/contents/ZepBlocks.java
+++ src/zephirum/contents/ZepBlocks.java
@@ -2,14 +2,17 @@ package zephirum.contents;
 
 import arc.*;
 import arc.graphics.*;
+import arc.math.*;
 import mindustry.content.*;
-import mindustry.entities.bullet.SapBulletType;
+import mindustry.entities.bullet.*;
 import mindustry.gen.*;
 import mindustry.type.*;
 import mindustry.world.*;
+import mindustry.world.consumers.*;
 import mindustry.world.blocks.defense.*;
 import mindustry.world.blocks.defense.turrets.*;
-import mindustry.world.blocks.storage.CoreBlock;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.storage.*;
 
 import zephirum.contents.ZepItems.*;
 import zephirum.world.blocks.*;
@@ -23,6 +26,9 @@ public class ZepBlocks {
     // drill
     drill,
 
+    // ore
+    oreIridium,
+    
     // power generator
     clickGenerator,
 
@@ -33,6 +39,7 @@ public class ZepBlocks {
     coreFragment;
 
     public static void load() {
+        // region turret
         spiron = new ItemTurret("spiron"){{
             requirements(Category.turret, ItemStack.with(Items.lead, 80, Items.graphite, 65, Items.titanium, 50));
 
@@ -63,8 +70,24 @@ public class ZepBlocks {
                     ammoMultiplier = 3f;
                 }}
             );
+            
+            unitSort = (u, x, y) -> noSapped(u, x, y);
+            coolant = consume(new ConsumeLiquid(Liquids.water, 0.2f));
+            coolantMultiplier = 6.6f;
+        }
+
+        private float noSapped(Unit u, float x, float y) {
+            return u.isImmune(StatusEffects.sapped) ? 1000000f : (u.getDuration(StatusEffects.sapped) + Mathf.dst2(u.x, u.y, x, y) / 6400f);
         }};
 
+        // region ore
+        oreIridium = new OreBlock(ZepItems.iridium){{
+            oreDefault = true;
+            oreThreshold = 0.902f;
+            oreScale = 25.880953f;
+        }};
+
+        // region wall
         int wallHealthMultiplier = 4;
 
         iridiumWall = new Wall("iridium-wall"){{
@@ -72,20 +95,21 @@ public class ZepBlocks {
             health = 270 * wallHealthMultiplier;
             armor = 32f;
         }};
-
         iridiumWallLarge = new Wall("iridium-wall-large"){{
             requirements(Category.defense, ItemStack.mult(iridiumWall.requirements, 4));
             health = 270 * wallHealthMultiplier * 4;
             armor = 32f;
             size = 2;
         }};
-
+        
+        // region generator
         clickGenerator = new ClickGenerator("click-generator"){{
             requirements(Category.power, ItemStack.with(Items.graphite, 5));
             health = 40;
             powerProduction = 1f;
         }};
-
+        
+        // region core
         coreFragment = new CoreBlock("core-fragment"){{
             requirements(Category.effect, ItemStack.with(Items.copper, 1000, Items.lead, 800));
             alwaysUnlocked = true;
