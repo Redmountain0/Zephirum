f2ab0d81da15b4cedb7435f086572537bf3e40b5
Redmountain0 <65642577+Redmountain0@users.noreply.github.com>
2022-10-12 17:13:35 +0900

    test

--- src/zephirum/world/blocks/MultiCrafter.java
+++ src/zephirum/world/blocks/MultiCrafter.java
@@ -1,6 +1,10 @@
 package zephirum.world.blocks;
 
+import arc.util.Nullable;
+import mindustry.gen.Building;
 import mindustry.type.Item;
+import mindustry.type.ItemStack;
+import mindustry.world.Build;
 import mindustry.world.blocks.distribution.Sorter.SorterBuild;
 import mindustry.world.blocks.production.GenericCrafter;
 
@@ -10,9 +14,30 @@ public class MultiCrafter extends GenericCrafter {
         super(name);
         configurable = true;
         clearOnDoubleTap = true;
-        
-        config(Item.class, (SorterBuild tile, Item item) -> tile.sortItem = item);
-        configClear((SorterBuild tile) -> tile.sortItem = null);
+
+        config(CraftPlan.class, (MultiCrafterBuild tile, CraftPlan plan) -> tile.craftItem = plan);
+        configClear((MultiCrafterBuild tile) -> tile.craftItem = null);
     }
-    
+
+    public static class CraftPlan{
+        public ItemStack outputItem;
+        public ItemStack[] requirements;
+        public float time;
+
+        public CraftPlan(ItemStack outputItem, float time, ItemStack[] requirements){
+            this.outputItem = outputItem;
+            this.time = time;
+            this.requirements = requirements;
+        }
+
+        CraftPlan(){}
+    }
+
+    public class MultiCrafterBuild extends Building {
+        public @Nullable CraftPlan craftItem;
+        
+        public CraftPlan config(){
+            return craftItem;
+        }
+    }
 }
