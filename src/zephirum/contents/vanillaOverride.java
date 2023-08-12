d805e5ec95a08c18ab4aa23aca2af5122ac569d8
Redmountain0 <65642577+Redmountain0@users.noreply.github.com>
2023-08-12 17:35:44 +0900

    EI support add and wood fix

--- src/zephirum/contents/vanillaOverride.java
+++ src/zephirum/contents/vanillaOverride.java
@@ -1,12 +1,21 @@
 package zephirum.contents;
 
-import mindustry.content.Blocks;
-import mindustry.content.Items;
+import mindustry.Vars;
+import mindustry.content.*;
+import mindustry.ctype.ContentType;
 
 public class vanillaOverride {
     public static void load() {
+        // Vars.content.getByName(ContentType.item, "zephirum-wood");
         Blocks.sporePine.itemDrop = ZepItems.wood;
         Blocks.snowPine.itemDrop = ZepItems.wood;
         Blocks.pine.itemDrop = ZepItems.wood;
+        UnitTypes.alpha.mineWalls = true;
+        UnitTypes.beta.mineWalls = true;
+        UnitTypes.gamma.mineWalls = true;
+
+        // EI Support
+        Vars.content.getByName(ContentType.unit, "ei-piece").mineWalls = true;
+        Vars.content.getByName(ContentType.unit, "ei-piece").mineTier = 0;
     }
 }
